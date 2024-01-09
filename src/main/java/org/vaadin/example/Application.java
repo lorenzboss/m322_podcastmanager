7004522dad88bd956acdc4cf981d14759dd9a31f
lorenzboss <lorenz.boss@bbzbl-it.ch>
2024-01-09 17:49:29 +0100

    Application.java angepasst

--- src/main/java/org/vaadin/example/Application.java
+++ src/main/java/org/vaadin/example/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The entry point of the Spring Boot application.
- * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
  */
@@ -20,5 +19,4 @@ public class Application implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
